70d0b11d447c40a0db056d49689310f789599c22
ddmendoza0 <ddmendozafernandez@gmail.com>
2026-02-13 19:20:25 +0100

    minimap dynamic sizeing

--- src/game/minimap.c
+++ src/game/minimap.c
@@ -1,20 +1,29 @@
+static int	calculate_scale(t_game *game)
+{
+	int	scale_x;
+	int	scale_y;
 
-#include "cub3d.h"
+	scale_x = (MINIMAP_SIZE - 10) / game->map.width;
+	scale_y = (MINIMAP_SIZE - 10) / game->map.height;
+	if (scale_x < scale_y)
+		return (scale_x < 1 ? 1 : scale_x);
+	return (scale_y < 1 ? 1 : scale_y);
+}
 
-static void	draw_minimap_tile(t_game *game, int x, int y, uint32_t color)
+static void	draw_minimap_tile(t_game *game, int x, int y, uint32_t color, int scale)
 {
 	int	i;
 	int	j;
 	int	screen_x;
 	int	screen_y;
 
-	screen_x = MINIMAP_PADDING + (x * MINIMAP_SCALE);
-	screen_y = MINIMAP_PADDING + (y * MINIMAP_SCALE);
+	screen_x = MINIMAP_PADDING + (x * scale);
+	screen_y = MINIMAP_PADDING + (y * scale);
 	j = 0;
-	while (j < MINIMAP_SCALE)
+	while (j < scale)
 	{
 		i = 0;
-		while (i < MINIMAP_SCALE)
+		while (i < scale)
 		{
 			if (screen_x + i < WIDTH && screen_y + j < HEIGHT)
 				pixel_put(game, screen_x + i, screen_y + j, color);
@@ -24,18 +33,25 @@ static void	draw_minimap_tile(t_game *game, int x, int y, uint32_t color)
 	}
 }
 
-void draw_minimap(t_game* game)
+void	draw_minimap(t_game *game)
 {
 	int			x;
 	int			y;
+	int			player_x;
+	int			player_y;
 	uint32_t	color;
+	int			scale;
 
-
+	if (!game || !game->map.grid || game->map.height == 0)
+		return;
+	scale = calculate_scale(game);
+	player_x = (int)(game->player.x / BLOCK);
+	player_y = (int)(game->player.y / BLOCK);
 	y = 0;
-	while (y < game->map.height && y * MINIMAP_SCALE < MINIMAP_SIZE)
+	while (y < game->map.height)
 	{
 		x = 0;
-		while (x < game->map.width && x * MINIMAP_SCALE < MINIMAP_SIZE)
+		while (x < game->map.width)
 		{
 			if (game->map.grid[y][x] == '1')
 				color = 0x000000FF;
@@ -43,9 +59,9 @@ void draw_minimap(t_game* game)
 				color = 0x80808080;
 			else
 				color = 0xFFFFFFFF;
-			if ((int)game->player.x / BLOCK == x && (int)game->player.y / BLOCK == y)
+			if (player_x == x && player_y == y)
 				color = 0xFF0000FF;
-			draw_minimap_tile(game, x, y, color);
+			draw_minimap_tile(game, x, y, color, scale);
 			x++;
 		}
 		y++;
