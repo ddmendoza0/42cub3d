b17de391352b45885a9489a0b8781b20f34e57d9
ddmendoza0 <ddmendozafernandez@gmail.com>
2026-03-23 07:37:55 +0100

    minimap

--- src/game/minimap.c
+++ src/game/minimap.c
@@ -55,6 +55,48 @@ static void	draw_mm_pixel(t_game *game, int dx, int dy)
 	pixel_put(game, screen_x, screen_y, color);
 }
 
+static void	draw_mm_border(t_game *game)
+{
+	int	dx;
+	int	dy;
+	int	dist;
+
+	dy = -MM_RADIUS - 2;
+	while (dy <= MM_RADIUS + 2)
+	{
+		dx = -MM_RADIUS - 2;
+		while (dx <= MM_RADIUS + 2)
+		{
+			dist = dx * dx + dy * dy;
+			if (dist >= (MM_RADIUS) * (MM_RADIUS)
+				&& dist <= (MM_RADIUS + 2) * (MM_RADIUS + 2))
+				pixel_put(game, MINIMAP_PADDING + MM_RADIUS + dx,
+					MINIMAP_PADDING + MM_RADIUS + dy, 0xFFFFFFFF);
+			dx++;
+		}
+		dy++;
+	}
+}
+
+static void	draw_mm_player(t_game *game)
+{
+	int	cx;
+	int	cy;
+
+	cx = MINIMAP_PADDING + MM_RADIUS;
+	cy = MINIMAP_PADDING + MM_RADIUS;
+	pixel_put(game, cx, cy - 4, 0xFF0000FF);
+	pixel_put(game, cx - 1, cy - 3, 0xFF0000FF);
+	pixel_put(game, cx, cy - 3, 0xFF0000FF);
+	pixel_put(game, cx + 1, cy - 3, 0xFF0000FF);
+	pixel_put(game, cx - 2, cy - 2, 0xFF0000FF);
+	pixel_put(game, cx, cy - 2, 0xFF0000FF);
+	pixel_put(game, cx + 2, cy - 2, 0xFF0000FF);
+	pixel_put(game, cx, cy - 1, 0xFF0000FF);
+	pixel_put(game, cx, cy, 0xFF0000FF);
+	pixel_put(game, cx, cy + 1, 0xFF0000FF);
+}
+
 void	draw_minimap(t_game *game)
 {
 	int	dx;
@@ -74,93 +116,6 @@ void	draw_minimap(t_game *game)
 		}
 		dy++;
 	}
-	pixel_put(game, MINIMAP_PADDING + MM_RADIUS - 1, MINIMAP_PADDING + MM_RADIUS - 1, 0xFF0000FF);
-	pixel_put(game, MINIMAP_PADDING + MM_RADIUS, MINIMAP_PADDING + MM_RADIUS - 1, 0xFF0000FF);
-	pixel_put(game, MINIMAP_PADDING + MM_RADIUS + 1, MINIMAP_PADDING + MM_RADIUS - 1, 0xFF0000FF);
-	pixel_put(game, MINIMAP_PADDING + MM_RADIUS - 1, MINIMAP_PADDING + MM_RADIUS, 0xFF0000FF);
-	pixel_put(game, MINIMAP_PADDING + MM_RADIUS, MINIMAP_PADDING + MM_RADIUS, 0xFF0000FF);
-	pixel_put(game, MINIMAP_PADDING + MM_RADIUS + 1, MINIMAP_PADDING + MM_RADIUS, 0xFF0000FF);
-	pixel_put(game, MINIMAP_PADDING + MM_RADIUS - 1, MINIMAP_PADDING + MM_RADIUS + 1, 0xFF0000FF);
-	pixel_put(game, MINIMAP_PADDING + MM_RADIUS, MINIMAP_PADDING + MM_RADIUS + 1, 0xFF0000FF);
-	pixel_put(game, MINIMAP_PADDING + MM_RADIUS + 1, MINIMAP_PADDING + MM_RADIUS + 1, 0xFF0000FF);
+	draw_mm_border(game);
+	draw_mm_player(game);
 }
-//#include "cub3d.h"
-//
-//static int	calculate_scale(t_game *game)
-//{
-//	int	scale_x;
-//	int	scale_y;
-//	int	scale;
-//
-//	scale_x = (MINIMAP_SIZE - 10) / game->map.width;
-//	scale_y = (MINIMAP_SIZE - 10) / game->map.height;
-//	if (scale_x < scale_y)
-//		scale = scale_x;
-//	else
-//		scale = scale_y;
-//	if (scale < 1)
-//		scale = 1;
-//	return (scale);
-//}
-//
-//static void	draw_minimap_tile(t_game *game, int x, int y, uint32_t color, int scale)
-//{
-//	int	i;
-//	int	j;
-//	int	screen_x;
-//	int	screen_y;
-//
-//	screen_x = scale * x;
-//	screen_y = scale * y;
-//	screen_x += WIDTH - (game->map.width * scale + MINIMAP_PADDING);
-//	screen_y += HEIGHT - (game->map.height * scale + MINIMAP_PADDING);
-//	j = 0;
-//	while (j < scale)
-//	{
-//		i = 0;
-//		while (i < scale)
-//		{
-//			if (screen_x + i < WIDTH && screen_y + j < HEIGHT)
-//				pixel_put(game, screen_x + i, screen_y + j, color);
-//			i++;
-//		}
-//		j++;
-//	}
-//}
-//
-//void	draw_minimap(t_game *game)
-//{
-//	int			x;
-//	int			y;
-//	int			player_x;
-//	int			player_y;
-//	uint32_t	color;
-//	int			scale;
-//
-//	if (!game || !game->map.grid || game->map.height == 0)
-//		return ;
-//	scale = calculate_scale(game);
-//	player_x = (int)(game->player.x / BLOCK);
-//	player_y = (int)(game->player.y / BLOCK);
-//	y = 0;
-//	while (y < game->map.height)
-//	{
-//		x = 0;
-//		while (x < game->map.width)
-//		{
-//			if (game->map.grid[y][x] == '1')
-//				color = 0x000000FF;
-//			else if (game->map.grid[y][x] == ' ')
-//				color = 0x80808080;
-//			else if (game->map.grid[y][x] == 'D')
-//				color = 0x00FF00FF;
-//			else
-//				color = 0xFFFFFFFF;
-//			if (player_x == x && player_y == y)
-//				color = 0xFF0000FF;
-//			draw_minimap_tile(game, x, y, color, scale);
-//			x++;
-//		}
-//		y++;
-//	}
-//}
