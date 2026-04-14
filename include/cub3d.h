b59b2ef2222e164fdae1365e510b138492d75784
ddmendoza0 <ddmendozafernandez@gmail.com>
2026-04-14 08:22:41 +0200

    norminette - function scope corrected

--- include/cub3d.h
+++ include/cub3d.h
@@ -135,6 +135,7 @@ int		parse_identifier(char *line, t_game *game);
 int		parse_color(char *line, t_colors *colors, int is_floor);
 int		validate_identifiers(t_game *game);
 int		parse_map(int fd, t_game *game, char *first_line);
+int		is_player(char c);
 int		validate_texture_file(char *path);
 int		validate_map_characters(t_game *game);
 int		validate_map_closed(t_game *game);
--- src/parsing/validate_map_chars.c
+++ src/parsing/validate_map_chars.c
@@ -17,7 +17,7 @@ static int	is_valid_char(char c)
 	return (c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'E' || c == 'W' || c == ' ' || c == 'D');
 }
 
-static int	is_player(char c)
+int	is_player(char c)
 {
 	return (c == 'N' || c == 'S' || c == 'E' || c == 'W');
 }
